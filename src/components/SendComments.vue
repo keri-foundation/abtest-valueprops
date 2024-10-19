b2322476e9c029049817ddb07e61033dc0c2c7dd
kor <kor@dwarshuis.com>
2024-10-19 20:00:49 +0200

    Component that adds comments to localStorage

---
+++ src/components/SendComments.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>If you have comments, please post them here.</p>
+    <div class="form-group">
+        <textarea class="form-control" v-model="comments" placeholder="Enter your comments here"
+            maxlength="1000"></textarea>
+    </div>
+    <button class="btn btn-outline-primary btn-sm mt-2" @click="submitComment">Submit</button>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+import { useMainStore } from '@/stores/mainStore';
+
+const store = useMainStore();
+const comments = ref('');
+const allMultipleChoiceAnswered = computed(() => store.allMultipleChoiceAnswered);
+
+const submitComment = () => {
+    console.log('Comment submitted');
+    store.addToResults("Comments: " + comments.value);
+    comments.value = ''; // Clear the textarea after submission
+}
+</script>
