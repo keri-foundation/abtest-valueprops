82c88312514590f03954340af016bb2c1e181174
kor <kor@dwarshuis.com>
2024-10-22 20:40:32 +0200

    Add repo name in production path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // base: mode === 'production' ? '/test/cylinder-lock-vue/' : '/',
-  base: mode === 'production' ? '/' : '/',
+  base: mode === 'production' ? '/abtest-valueprops/' : '/',
 
   plugins: [
     vue(),
