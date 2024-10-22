322d3e4c1301466cc479441c2109b28f2a4e9d3f
kor <kor@dwarshuis.com>
2024-10-22 20:18:50 +0200

    Set path in production

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: mode === 'production' ? 'test/cylinder-lock-vue/' : '',
-  base: mode === 'production' ? '/test/cylinder-lock-vue/' : '/',
+  // base: mode === 'production' ? '/test/cylinder-lock-vue/' : '/',
+  base: mode === 'production' ? '/' : '/',
 
   plugins: [
     vue(),
