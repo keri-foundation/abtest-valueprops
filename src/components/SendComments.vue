8ae38cc001ed051f6882acd10815f86b54e0e3c2
kor <kor@dwarshuis.com>
2024-10-23 22:16:08 +0200

    Make it a modal

--- src/components/SendComments.vue
+++ src/components/SendComments.vue
@@ -1,24 +1,67 @@
 <template>
     <p>If you have comments, please post them here.</p>
-    <div class="form-group">
-        <textarea class="form-control" v-model="comments" placeholder="Enter your comments here"
-            maxlength="1000"></textarea>
+
+    <div class="modal fade" ref="commentsModal" tabindex="-1" aria-labelledby="commentsModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content commentsModal">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="commentsModalLabel">If you have comments about these statements and question, please post them here.</h5>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <textarea class="form-control" v-model="comments" placeholder="Enter your comments here"
+                            maxlength="1000"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-outline-primary btn-sm mt-2" @click="submitComment">Submit</button>
+                </div>
+            </div>
+        </div>
     </div>
-    <button class="btn btn-outline-primary btn-sm mt-2" @click="submitComment">Submit</button>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
+import { Modal } from 'bootstrap';
 import { useMainStore } from '@/stores/mainStore';
-
+const commentsModal = ref(null);
+let modalComments;
 const store = useMainStore();
+const contentOfChosenMultipleChoiceAnswer = computed(() => store.contentOfChosenMultipleChoiceAnswer);
 const comments = ref('');
-const allMultipleChoiceAnswered = computed(() => store.allMultipleChoiceAnswered);
+import { useSounds } from '@/composables/useSounds';
+const { electricSound } = useSounds();
+
+// Hacky solution to remove existing backdrops
+const removeExistingBackdrops = () => {
+    const backdrops = document.querySelectorAll('.modal-backdrop');
+    backdrops.forEach(backdrop => backdrop.remove());
+};
 
 const submitComment = () => {
     console.log('Comment submitted');
     store.addToResultsInLocalStorage("Comments: " + comments.value);
     store.updateLedger({ id: store.nrOfStatementsChosen, comments: comments.value });
     comments.value = ''; // Clear the textarea after submission
+    modalComments.hide();
+    store.setShouldNewStatementShow(true);
 }
+
+watch(
+    () => contentOfChosenMultipleChoiceAnswer.value,
+    (newVal, oldVal) => {
+        modalComments = new Modal(commentsModal.value, {
+            backdrop: 'static',
+            keyboard: false
+        });
+        setTimeout(() => {
+            removeExistingBackdrops();
+            modalComments.show();
+            setTimeout(() => {
+                electricSound.play();
+            }, 100);
+        }, 30);
+    }
+);
 </script>
