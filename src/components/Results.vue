d436e6e487ebaabc2edc59943025d5e8f0c56ab0
kor <kor@dwarshuis.com>
2024-10-24 19:54:11 +0200

    Make comments editable

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -6,9 +6,7 @@
                 <div class="modal-header">
                     <h5 class="modal-title" id="resultsModalLabel">Results</h5>
                 </div>
-                <!-- Loop through store.ledger -->
-                <div class="modal-body
-                ">
+                <div class="modal-body">
                     <div v-for="(item, index) in store.ledger" :key="index">
                         <h2>Set {{ item.id }}</h2>
                         <h3>Chosen statement</h3>
@@ -16,7 +14,11 @@
                         <h3>Chosen multiple choice answer</h3>
                         <p>{{ item.chosenMultipleChoiceAnswer }}</p>
                         <h3>Comment</h3>
-                        <p>{{ item.comments }}</p>
+                        <textarea :disabled="!isEditing[item.id]" class="form-control"
+                            v-model="comments[item.id]"></textarea>
+                        <button :disabled="!isEditing[item.id]" class="btn btn-outline-primary btn-sm mt-2"
+                            @click="saveComments(item.id)">Save</button>
+                        <button :disabled="isEditing[item.id]" class="btn btn-outline-primary btn-sm mt-2 ms-2" @click="enableEdit(item.id)">Edit</button>
                         <hr>
                     </div>
                 </div>
@@ -29,10 +31,9 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive, watch } from 'vue';
 import { Modal } from 'bootstrap';
 import { useMainStore } from '../stores/mainStore.js';
-import { useResults } from '../composables/useResults';
 
 let modal;
 const resultsFromLocalStorageModal = ref(null);
@@ -41,16 +42,42 @@ const hideModal = () => {
 };
 
 const store = useMainStore();
-// const { resultsFromLocalStorage, loadResults } = useResults();
 
+// Create a reactive object to store comments for each item
+const comments = reactive({});
+const isEditing = reactive({});
 
+// Initialize comments and edit state with existing values from the store
+store.ledger.forEach(item => {
+    comments[item.id] = item.comments;
+    isEditing[item.id] = false; // Initially, all textareas are not in edit mode
+});
+
+// Watch the getter `ledgerArray` for changes
+watch(() => store.ledgerArray, (newValue, oldValue) => {
+    newValue.forEach(item => {
+        comments[item.id] = item.comments;
+    });
+}, { deep: true });
 
 const showResults = () => {
     modal = new Modal(resultsFromLocalStorageModal.value);
     setTimeout(() => {
         modal.show();
     }, 1);
+};
 
+const saveComments = (id) => {
+    store.updateLedger({ id, comments: comments[id] });
+    disableEdit(id);
+};
+
+const enableEdit = (id) => {
+    isEditing[id] = true;
+};
+
+const disableEdit = (id) => {
+    isEditing[id] = false;
 };
 
 defineExpose({
